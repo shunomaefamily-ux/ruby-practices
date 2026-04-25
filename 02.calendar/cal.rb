b48abc8c66f89379dd41124f99f915252402e956
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-04-26 00:01:45 +0900

    OptionParserで-y,--yearと、同様に-m,--Monthの計2つのオプションを作成し、オプションの引数をDateの引数にした

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,5 +1,14 @@
 require 'date'
-a=Date.new(1997, 2, 1)
+require "optparse"
+opts = OptionParser.new
+opts.on("-y", "--year N1", Integer){|w1|
+  Option_Year = w1
+}
+opts.on("-m", "--month N2", Integer){|w2|
+  Option_Month = w2
+}
+opts.parse!(ARGV)
+a=Date.new(Option_Year, Option_Month, 1)
 Month_and_Year = a.strftime('%B')+" "+"#{a.year}"
 puts Month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
