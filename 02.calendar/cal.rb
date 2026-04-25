5cbf799b0bdad97aaafa17286146a1f617df86dd
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-04-25 22:29:50 +0900

    月の頭から尻の日付を7列にして表示する（サンプル：1997, 2, 1）

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,3 +3,14 @@ a=Date.new(1997, 2, 1)
 a.cwday.times do
   print"  "
 end
+days_in_month = a.next_month.jd - a.jd
+days_in_month.times do
+if a.cwday==6
+  puts "#{a.day}"
+  elsif a.day==days_in_month
+    puts "#{a.day}"
+    else
+    print "#{a.day}"+" "
+  end
+  a+=1
+end
