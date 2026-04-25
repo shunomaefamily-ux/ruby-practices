ac62a837622edc58897561724e823de9d06ca7e1
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-04-26 08:36:40 +0900

    上書きの警告を解決するために、2つの変数名を変更：Option_YearとOption_Monthをoption_yearとoption_monthに変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,16 +1,16 @@
 require 'date'
 require "optparse"
-Option_Year=Date.today.year
-Option_Month=Date.today.month
+option_year=Date.today.year
+option_month=Date.today.month
 opts = OptionParser.new
 opts.on("-y", "--year N1", Integer){|w1|
-  Option_Year = w1
+  option_year = w1
 }
 opts.on("-m", "--month N2", Integer){|w2|
-  Option_Month = w2
+  option_month = w2
 }
 opts.parse!(ARGV)
-a=Date.new(Option_Year, Option_Month, 1)
+a=Date.new(option_year, option_month, 1)
 Month_and_Year = a.strftime('%B')+" "+"#{a.year}"
 puts Month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
