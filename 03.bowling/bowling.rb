77395b4211ad2efa085368e5b2ab03d38ce03cd1
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-04-27 21:56:14 +0900

    スコア入力を直書きからARGV入力へ変更し、'X'を10として処理するように修正。また、それに合わせて変数ｘの名前を変数shotsに変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,25 +1,36 @@
-x = [0,10,1,5,0,0,0,0,10,10,10,5,1,8,1,0,4]
+score = ARGV[0]
+
+scores = score.split(',')
+
+shots = []
+scores.each do |s|
+  if s == 'X' # strike
+    shots << 10
+  else
+    shots << s.to_i
+  end
+end
 
 extra_sum=0
 a=0
 9.times do
-  puts x[a]
-  if x[a]==10
-    extra_sum = extra_sum + x[a+1] + x[a+2]
+  puts shots[a]
+  if shots[a]==10
+    extra_sum = extra_sum + shots[a+1] + shots[a+2]
     a+=1
    next
   else
-    if x[a] + x[a+1] == 10
-    extra_sum = extra_sum + x[a+2]
+    if shots[a] + shots[a+1] == 10
+    extra_sum = extra_sum + shots[a+2]
     end
     a+=1
-    puts x[a]
+    puts shots[a]
     a+=1
   end
 end
 
 
-puts x[a]
-puts x[a+1]
-puts x[a+2]
-puts extra_sum + x.sum
+puts shots[a]
+puts shots[a+1]
+puts shots[a+2]
+puts extra_sum + shots.sum
