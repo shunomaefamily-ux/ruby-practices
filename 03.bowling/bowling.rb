dec364d2317cbc57b56885d2c1d678c1c2b2c338
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-04-27 22:24:46 +0900

    rubocop-fjordで自動修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,29 +1,28 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
 shots = []
 
 scores.each do |s|
-  if s == 'X' # strike
-    shots << 10
-  else
-    shots << s.to_i
-  end
+  shots << if s == 'X' # strike
+             10
+           else
+             s.to_i
+           end
 end
 
-extra_sum=0
-a=0
+extra_sum = 0
+a = 0
 9.times do
-  if shots[a]==10
-    extra_sum = extra_sum + shots[a+1] + shots[a+2]
-    a+=1
-   next
+  if shots[a] == 10
+    extra_sum = extra_sum + shots[a + 1] + shots[a + 2]
+    a += 1
+    next
   else
-    if shots[a] + shots[a+1] == 10
-    extra_sum = extra_sum + shots[a+2]
-    end
-    a+=2
+    extra_sum += shots[a + 2] if shots[a] + shots[a + 1] == 10
+    a += 2
   end
 end
 
