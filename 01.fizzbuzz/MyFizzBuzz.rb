920aa2cf3dfc029abf68433e6ec2f390c441210f
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 18:37:43 +0900

    演算子の前後にはスペースがあるのが望ましいので、そのとおりに全文を修正

--- 01.fizzbuzz/MyFizzBuzz.rb
+++ 01.fizzbuzz/MyFizzBuzz.rb
@@ -1,9 +1,9 @@
 1.upto(20) do |x|
-  if x%15==0
+  if x % 15 == 0
     puts "FizzBuzz"
-    elsif x%3==0
+    elsif x % 3 == 0
       puts "Fizz"
-    elsif x%5==0
+    elsif x % 5 == 0
       puts "Buzz"
     else
       puts "#{x}"
