dbe82e9062d25ac138b28ceea41d02b366a405de
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 21:19:55 +0900

    書き方を整える（rubocopの指摘）：opts.onメソッドの引数、printの引数、文字列内の空白スペースをシングルクォートに変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,19 +5,19 @@ require 'optparse'
 option_year = Date.today.year
 option_month = Date.today.month
 opts = OptionParser.new
-opts.on("-y", "--year input_year", Integer){|optioned_year|
+opts.on('-y', '--year input_year', Integer){|optioned_year|
   option_year = optioned_year
 }
-opts.on("-m", "--month input_month", Integer){|optioned_month|
+opts.on('-m', '--month input_month', Integer){|optioned_month|
   option_month = optioned_month
 }
 opts.parse!(ARGV)
 day_of_this_month = Date.new(option_year, option_month, 1)
-month_and_Year = day_of_this_month.strftime('%B') + " " + "#{day_of_this_month.year}"
+month_and_Year = day_of_this_month.strftime('%B') + ' ' + "#{day_of_this_month.year}"
 puts month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
 day_of_this_month.cwday.times do
-  print"   "
+  print '   '
 end
 days_in_month = day_of_this_month.next_month.jd - day_of_this_month.jd
 days_in_month.times do
