4e0c762204155bfcd21131920da4669174092615
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 21:25:51 +0900

    書き方を整える（rubocopの指摘）：ヘッダーの曜日一覧をシングルクォートでの表記に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -15,7 +15,7 @@ opts.parse!(ARGV)
 day_of_this_month = Date.new(option_year, option_month, 1)
 month_and_Year = day_of_this_month.strftime('%B') + ' ' + "#{day_of_this_month.year}"
 puts month_and_Year.center(20)
-puts "Su Mo Tu We Th Fr Sa"
+puts 'Su Mo Tu We Th Fr Sa'
 day_of_this_month.cwday.times do
   print '   '
 end
