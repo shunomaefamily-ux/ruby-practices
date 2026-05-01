847dd31ad2808956d688b24670bb84838830a23c
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 18:30:09 +0900

    FizzとBuzzの条件分岐で不要だったので、x%5!=0とx%3!=0を削除

--- 01.fizzbuzz/MyFizzBuzz.rb
+++ 01.fizzbuzz/MyFizzBuzz.rb
@@ -1,9 +1,9 @@
 1.upto(20) do |x|
   if x%15==0
     puts "FizzBuzz"
-    elsif x%3==0&&x%5!=0
+    elsif x%3==0
       puts "Fizz"
-    elsif x%5==0&&x%3!=0
+    elsif x%5==0
       puts "Buzz"
     else puts "#{x}"
   end
