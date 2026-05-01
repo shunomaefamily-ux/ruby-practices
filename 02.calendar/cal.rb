b6fe4dc25439a243b8a81574141b7417866832e9
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 20:38:44 +0900

    コードを読みやすくするために、抽象的な変数名を具体的な名前に 変更：a→day_of_this_month　w1→optioned_year　w2→optioned_month　N1→input_year　N2→input_month

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,28 +3,28 @@ require "optparse"
 option_year=Date.today.year
 option_month=Date.today.month
 opts = OptionParser.new
-opts.on("-y", "--year N1", Integer){|w1|
-  option_year = w1
+opts.on("-y", "--year input_year", Integer){|optioned_year|
+  option_year = optioned_year
 }
-opts.on("-m", "--month N2", Integer){|w2|
-  option_month = w2
+opts.on("-m", "--month input_month", Integer){|optioned_month|
+  option_month = optioned_month
 }
 opts.parse!(ARGV)
-a=Date.new(option_year, option_month, 1)
-Month_and_Year = a.strftime('%B')+" "+"#{a.year}"
+day_of_this_month=Date.new(option_year, option_month, 1)
+Month_and_Year = day_of_this_month.strftime('%B')+" "+"#{day_of_this_month.year}"
 puts Month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
-a.cwday.times do
+day_of_this_month.cwday.times do
   print"   "
 end
-days_in_month = a.next_month.jd - a.jd
+days_in_month = day_of_this_month.next_month.jd - day_of_this_month.jd
 days_in_month.times do
-  if a.cwday==6
-  puts a.strftime('%e')
-    elsif a.day==days_in_month
-    puts a.strftime('%e')
+  if day_of_this_month.cwday==6
+  puts day_of_this_month.strftime('%e')
+    elsif day_of_this_month.day==days_in_month
+    puts day_of_this_month.strftime('%e')
     else
-    print a.strftime('%e')+" "
+    print day_of_this_month.strftime('%e')+" "
   end
-  a+=1
+  day_of_this_month+=1
 end
