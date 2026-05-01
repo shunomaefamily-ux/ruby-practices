ad00625e7dff98f52141d8b0a6cb71dfa5725cc4
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 21:05:23 +0900

    書き方を整える：演算子の前後にスペースを追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,8 +2,8 @@ require 'date'
 
 require 'optparse'
 
-option_year=Date.today.year
-option_month=Date.today.month
+option_year = Date.today.year
+option_month = Date.today.month
 opts = OptionParser.new
 opts.on("-y", "--year input_year", Integer){|optioned_year|
   option_year = optioned_year
@@ -12,8 +12,8 @@ opts.on("-m", "--month input_month", Integer){|optioned_month|
   option_month = optioned_month
 }
 opts.parse!(ARGV)
-day_of_this_month=Date.new(option_year, option_month, 1)
-month_and_Year = day_of_this_month.strftime('%B')+" "+"#{day_of_this_month.year}"
+day_of_this_month = Date.new(option_year, option_month, 1)
+month_and_Year = day_of_this_month.strftime('%B') + " " + "#{day_of_this_month.year}"
 puts month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
 day_of_this_month.cwday.times do
@@ -21,12 +21,12 @@ day_of_this_month.cwday.times do
 end
 days_in_month = day_of_this_month.next_month.jd - day_of_this_month.jd
 days_in_month.times do
-  if day_of_this_month.cwday==6
+  if day_of_this_month.cwday == 6
   puts day_of_this_month.strftime('%e')
-    elsif day_of_this_month.day==days_in_month
+    elsif day_of_this_month.day == days_in_month
     puts day_of_this_month.strftime('%e')
     else
     print day_of_this_month.strftime('%e')+" "
   end
-  day_of_this_month+=1
+  day_of_this_month += 1
 end
