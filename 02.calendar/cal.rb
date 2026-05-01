275f9619949da282421904b7de40a347db9f86c8
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-01 20:44:48 +0900

    Month_and_Yearは変数のため、大文字を回避して、month_and_Yearに名前を変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -11,8 +11,8 @@ opts.on("-m", "--month input_month", Integer){|optioned_month|
 }
 opts.parse!(ARGV)
 day_of_this_month=Date.new(option_year, option_month, 1)
-Month_and_Year = day_of_this_month.strftime('%B')+" "+"#{day_of_this_month.year}"
-puts Month_and_Year.center(20)
+month_and_Year = day_of_this_month.strftime('%B')+" "+"#{day_of_this_month.year}"
+puts month_and_Year.center(20)
 puts "Su Mo Tu We Th Fr Sa"
 day_of_this_month.cwday.times do
   print"   "
