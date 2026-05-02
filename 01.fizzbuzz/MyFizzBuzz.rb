d68e2f2e0c88b15d15a8589a516880891df278a5
shunomaefamily-ux <shun.omaefamily@gmail.com>
2026-05-02 09:05:11 +0900

    FizzBuzz問題が扱うのは整数のため、変数名ｘを変数名iに変更

--- 01.fizzbuzz/MyFizzBuzz.rb
+++ 01.fizzbuzz/MyFizzBuzz.rb
@@ -1,11 +1,11 @@
-1.upto(20) do |x|
-  if x % 15 == 0
+1.upto(20) do |i|
+  if i % 15 == 0
     puts "FizzBuzz"
-  elsif x % 3 == 0
+  elsif i % 3 == 0
     puts "Fizz"
-  elsif x % 5 == 0
+  elsif i % 5 == 0
     puts "Buzz"
   else
-    puts "#{x}"
+    puts "#{i}"
   end
 end
